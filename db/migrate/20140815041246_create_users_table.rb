5f2475113345bd2e5adbc483640700b223b58d9a
Kevin Ko <enhasa@gmail.com>
2014-08-17 16:26:32 -0700

    Readd name column to original users migration so future migration that removes it can pass

--- db/migrate/20140815041246_create_users_table.rb
+++ db/migrate/20140815041246_create_users_table.rb
@@ -3,6 +3,7 @@ class CreateUsersTable < ActiveRecord::Migration
     create_table :users do |t|
       t.string :username, null: false, unique: true
       t.string :email
+      t.string :name
       t.string :admin, default: false
       t.string :password_digest, null: false
       t.string :password_salt, null: false
