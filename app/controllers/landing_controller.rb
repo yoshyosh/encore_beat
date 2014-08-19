7b2e324c37ce40e9d5865faed4040101ad1d6d24
Kevin Ko <enhasa@gmail.com>
2014-08-19 12:50:18 -0700

    Revise landing page for optimization purposes

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -7,6 +7,10 @@ class LandingController < ApplicationController
       .limit(50)
       .group_by(&:published_at)
 
+    if current_user
+      @current_user_upvotes = current_user.upvotes
+    end
+
     @upvote_counts = Upvote.valid.group_by(&:submission_id)
   end
 end
