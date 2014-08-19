9f86c460480c5af334632277c991bb70c06c83e6
Kevin Ko <enhasa@gmail.com>
2014-08-19 11:46:04 -0700

    Sync upvotes with vote count in real-time

--- app/assets/javascripts/react_components/upvote.js.jsx
+++ app/assets/javascripts/react_components/upvote.js.jsx
@@ -15,6 +15,16 @@ var Upvote = React.createClass({
 
   handleClick: function() {
     this.setState({clicked: !this.state.clicked});
+    var counter = $('.vote-count[data-count-id=' + this.props.song_id + ']')
+    var currentCount = parseInt(counter.text())
+
+    if (this.state.clicked) {
+      // state has not yet updated, so we use the inverse. Deduct if state is "currently" clicked
+      counter.text(currentCount - 1)
+    }
+    else {
+      counter.text(currentCount + 1)
+    }
 
     $.ajax({
       data: {submission_id: this.props.song_id, upvoted: !this.state.clicked },
