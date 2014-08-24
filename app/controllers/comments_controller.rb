ab5b27402c3e5e0dd00f95adcdff8e9f2bd1d962
Kevin Ko <enhasa@gmail.com>
2014-08-23 21:07:57 -0700

    Properly count comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,19 +1,27 @@
 class CommentsController < ApplicationController
-
   def create
     @comment = Comment.new(comment_params)
     @comment.user = current_user
-    @comment.save
 
-    if request.xhr?
-      render :json => @comment.submission.comments
+    if @comment.save
+      @comment.submission.submission_counts.comments += 1
+
+      if request.xhr?
+        render :json => @comment.submission.comments
+      else
+        redirect_to submission_path(submission)
+      end
     else
-      redirect_to submission_path(submission)
+      if request.xhr?
+        render :json => {fail: true}
+      else
+        flash[:error] = "Something went wrong with posting your comment. Comments have a 1000-character maximum length."
+        redirect_to submission_path(submission)
+      end
     end
   end
 
   def comment_params
     params.require(:comment).permit(:body, :submission_id)
   end
-
 end
