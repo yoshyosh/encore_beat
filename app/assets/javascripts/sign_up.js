3180d320e2de4952dc2209120b8b50acbeb3e019
yoshyosh <janderson@gmail.com>
2014-08-26 02:28:49 -0700

    added js based upload photo btn

--- app/assets/javascripts/sign_up.js
+++ app/assets/javascripts/sign_up.js
@@ -1,5 +1,15 @@
-$(".js-get-file-name").on("change", function(){
-  console.log("changed");
-  var filename = $(this).val().split('\\').pop();
-  console.log(filename);
-});
+function addFileNameToPage(){
+  var filename = $(".js-get-file-name").val().split('\\').pop();
+  if (filename != "") {
+    $(".js-get-file-name").hide();
+    $(".js-add-filename").text(filename);
+    $(".js-add-filename").append("<span class='close-icon'>&#10006;</span>");
+    $(".js-add-filename").show();
+  }
+}
+
+
+function hideFileNameHolder(){
+  $(".js-hide-avatar-added-text").hide();
+  $(".js-get-file-name").show();
+}
