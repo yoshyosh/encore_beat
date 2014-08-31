b7e71cf6c330fc12cc196bc58626b15892bd1e50
heyimkko <enhasa@gmail.com>
2014-08-31 15:20:12 -0700

    Guard against upvote hackery

--- app/controllers/upvotes_controller.rb
+++ app/controllers/upvotes_controller.rb
@@ -1,6 +1,6 @@
 class UpvotesController < ApplicationController
   def create
-    render status: 422 and return unless current_user
+    render status: 422 and return unless current_user && !request.xhr
     submission_id = params[:submission_id]
     is_upvoted = params[:upvoted] == "true"
     upvote = Upvote.where(submission_id: submission_id, user_id: current_user.id).first_or_initialize
@@ -8,16 +8,16 @@ class UpvotesController < ApplicationController
     upvote_exists = upvote.persisted?
 
     if upvote_exists
-      if is_upvoted
+      if is_upvoted && upvote.nullified == true
         # User upvoting a previously nullified upvote
-        count.increment_upvotes!
         upvote.nullified = false
         upvote.save
-      else
+        count.increment_upvotes!
+      elsif upvote.nullified == false
         # User nullifying their upvote
-        count.deduct_upvotes!
         upvote.nullified = true
         upvote.save
+        count.deduct_upvotes!
       end
     else
       # User upvoted, but the upvote record doesn't exist
