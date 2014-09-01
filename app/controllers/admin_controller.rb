f695d9d69b32c3c2bdbfaa3e5f753ebc38bed5a5
heyimkko <enhasa@gmail.com>
2014-09-01 16:37:19 -0700

    Use admin index page as default admin link. Set up site admins. Set up user list for siteadmins

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,5 +1,9 @@
 class AdminController < ApplicationController
   before_filter :authorize
+  before_filter :authorize_site_admin, only: :users
+
+  def index
+  end
 
   def approval_queue
     @submissions = 
@@ -11,9 +15,17 @@ class AdminController < ApplicationController
     @future_submissions = Submission.where('published_at = ?', Date.tomorrow).order('created_at DESC')
   end
 
+  def users
+    @users = User.order('created_at DESC').paginate(page: params[:page], :per_page => 5)
+  end
+
   private
 
   def authorize
     redirect_to root_path unless current_user && current_user.admin
+  end
+
+  def authorize_site_admin
+    redirect_to root_path unless current_user && current_user.site_admin
   end
 end
