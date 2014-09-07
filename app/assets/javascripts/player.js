50b7abb31e03ce17b4f1fb0d3c15c9e681c0eda3
yoshyosh <janderson@gmail.com>
2014-09-07 02:36:14 -0700

    Added sound cloud looping player

--- app/assets/javascripts/player.js
+++ app/assets/javascripts/player.js
@@ -32,22 +32,25 @@ $(document).ready(function(){
   // 3. This function creates an <iframe> (and YouTube player)
   //    after the API code downloads.
   var player;
-  loadYoutubeIframe();
+  var arrayOfSongIds = ["J6sRmvwSHQA", "J6sRmvwSHQA", "J6sRmvwSHQA"];
+
+  //loadYoutubeIframe();
   function loadYoutubeIframe(videoId){
     window.onYouTubeIframeAPIReady = function(){
       player = new YT.Player('player', {
         height: '115',
         width: '200',
         videoId: 'M7lc1UVf-VE',
-        playerVars: {
-          controls: 0
-        },
+        // playerVars: {
+        //   controls: 0
+        // },
         events: {
           'onReady': onPlayerReady,
           'onStateChange': onPlayerStateChange
         }
       });
     }
+    //Set current id on player
   }
 
   // 4. The API will call this function when the video player is ready.
@@ -55,28 +58,94 @@ $(document).ready(function(){
     event.target.playVideo();
   }
 
-  // 5. The API calls this function when the player's state changes.
-  //    The function indicates that when playing a video (state=1),
-  //    the player should play for six seconds and then stop.
-  var done = false;
   function onPlayerStateChange(event) {
     // Check here whether to show play or pause button
-    if (event.data == YT.PlayerState.PLAYING && !done) {
-      setTimeout(stopVideo, 6000);
-      done = true;
+    // -1 = unstarted
+    // 0 = ended
+    // 1 = playing
+    // 2 = paused
+    // 3 = buffering
+    var currentState = player.getPlayerState();
+    console.log(currentState);
+    if (currentState == 0) {
+      loadNextSong();
     }
-  }
-  function stopVideo() {
-    player.stopVideo();
+
   }
 
   // Player controls
-  $("#play-button").on("click", function(){
-    player.playVideo();
+  // $("#play-button").on("click", function(){
+  //   player.playVideo();
+  // });
+
+  // $("#stop-button").on("click", function(){
+  //   player.pauseVideo();
+  // });
+
+  // $("#next-button").on("click", function(){
+  //   loadNextSong();
+  // });
+
+  function loadNextSong(){
+    //Check array if next song exists
+    // If next song does not exist, go back to start of array
+    // also if player exists in dom just load video, otherwise recreate a new player iframe to replace soundclouds
+    // we will need to store state on the container rather than the player because of this
+    player.loadVideoById("J6sRmvwSHQA");
+  }
+
+  // Sound cloud player
+      var songFrame = document.createElement('iframe');
+    songFrame.src = "https://w.soundcloud.com/player/?url=https://soundcloud.com/dvbbs/dvbbs-joey-dale-deja-vu-ft-delora-available-september-19&auto_play=true";
+    songFrame.width = 200;
+    songFrame.height = 166;
+    songFrame.scrolling = "no";
+    songFrame.style["border"] = "none";
+    songFrame.id = "sc-widget";
+
+    $("#player").html(songFrame);
+
+    var widgetIframe = document.getElementById('sc-widget'),
+            widget       = SC.Widget(widgetIframe);
+    
+    widget.bind(SC.Widget.Events.READY, function() {
+          widget.bind(SC.Widget.Events.PLAY, function() {
+            // get information about currently playing sound
+            widget.getCurrentSound(function(currentSound) {
+              //alert('sound ' + currentSound.get('') + 'began to play');
+            });
+          });
+          // get current level of volume
+          widget.getVolume(function(volume) {
+            console.log('current volume value is ' + volume);
+          });
+          // set new volume level
+          widget.setVolume(50);
+          // get the value of the current position
+    });
+    
+    //song ended
+    widget.bind(SC.Widget.Events.FINISH, function(){
+        loadNextScSong();
+    });
+
+  //load in new song
+  $(".js-sc-next-button").on("click", function(){
+      loadNextScSong();
   });
 
-  $("#stop-button").on("click", function(){
-    player.stopVideo();
+  //pause song
+  $(".js-sc-pause-button").on("click", function(){
+      widget.pause();
   });
+
+  //play song
+  $(".js-sc-play-button").on("click", function(){
+      widget.play();
+  });
+      
+  function loadNextScSong(){
+     widget.load("https://soundcloud.com/anjunadeep/16-bit-lolitas-deep-in-my-soul-1&auto_play=true");
+  }
 
 });
