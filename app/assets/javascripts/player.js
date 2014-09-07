52fe7a44b179fdf2792b2c02261d299e5a1482b3
yoshyosh <janderson@gmail.com>
2014-09-06 23:54:36 -0700

    Added fixed youtube player to bottom right

--- app/assets/javascripts/player.js
+++ app/assets/javascripts/player.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+
+  // Comments page player
   var url = $(".preview-player a").attr("href");
   var a = document.createElement('a');
   a.href = url;
@@ -19,4 +21,48 @@ $(document).ready(function(){
       query: {maxwidth:530}
     });
   }
+
+  // Iframe real time player
+
+  var tag = document.createElement('script');
+  tag.src = "https://www.youtube.com/iframe_api";
+  var firstScriptTag = document.getElementsByTagName('script')[0];
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+  // 3. This function creates an <iframe> (and YouTube player)
+  //    after the API code downloads.
+  var player;
+  window.onYouTubeIframeAPIReady = function(){
+    player = new YT.Player('player', {
+      height: '115',
+      width: '200',
+      videoId: 'M7lc1UVf-VE',
+      events: {
+        'onReady': onPlayerReady,
+        'onStateChange': onPlayerStateChange
+      }
+    });
+  }
+
+  // 4. The API will call this function when the video player is ready.
+  function onPlayerReady(event) {
+    event.target.playVideo();
+  }
+
+  // 5. The API calls this function when the player's state changes.
+  //    The function indicates that when playing a video (state=1),
+  //    the player should play for six seconds and then stop.
+  var done = false;
+  function onPlayerStateChange(event) {
+    if (event.data == YT.PlayerState.PLAYING && !done) {
+      setTimeout(stopVideo, 6000);
+      done = true;
+    }
+  }
+  function stopVideo() {
+    player.stopVideo();
+  }
+
+
+
 });
