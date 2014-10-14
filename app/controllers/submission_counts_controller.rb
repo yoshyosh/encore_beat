465eeda68a5f70074d517fb3964ab700d0bdb2bf
heyimkko <enhasa@gmail.com>
2014-10-14 00:18:21 -0700

    Dont count site admin clicks

--- app/controllers/submission_counts_controller.rb
+++ app/controllers/submission_counts_controller.rb
@@ -1,5 +1,7 @@
 class SubmissionCountsController < ApplicationController
   def update
+    render json: {} and return if current_user && current_user.site_admin
+
     count = SubmissionCount.find_by_submission_id(edit_params[:id])
     count.clicks += 1 if count
     count.save
